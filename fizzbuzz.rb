48204726051f37d55d9c97ea2116ea20066a56c0
Bethany Jenkins <bethanykjenkins@rocketmail.com>
2020-08-24 22:16:48 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,10 +4,10 @@ def fizzbuzz(int)
   if int % 3 == 0 
     "Fizz"
   
-  else int % 5 == 0 
+  elseif int % 5 == 0 
     "Buzz"
     
-  else int % 5 == 0 && int % 3 == 0
+  elseif int % 15 == 0 
     "FizzBuzz"
   end
 end
